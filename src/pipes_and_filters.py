fcf72f44bd17a47585511c95360c790581ceee14
Lynne Williams <williams.lynne99@gmail.com>
2017-01-11 12:12:52 -0800

    everything works on multiple text files and starts and stops with streaming input flag. Included a generator for outputting the text file as part of reader. #1

--- src/pipes_and_filters.py
+++ src/pipes_and_filters.py
@@ -7,13 +7,6 @@ e.g.,
     results = p.run()
 """
 
-import numpy as np
-import time
-
-np.set_printoptions(threshold=np.inf)
-
-
-
 class Filter():
     """Base class that knows how to read and write data streams. Likely to end up as a queue of some sort"""
     def __init__(self):
@@ -29,24 +22,30 @@ class Filter():
 class Reader(Filter):
     """Class that produces output of Filter. Returns single record.
     """
-    def __init__(self, text):
+    def __init__(self, text='haiku.txt'):
         Filter.__init__(self)
-        self.file = text
+        self.file = []
         self.record = []
+        self.text = text
         
         
     def __iter__(self):
-        with open(self.file) as file:
-            for line in file:
+        with open(self.text) as self.file:
+            for line in self.file:
                 time.sleep(.25)
-                self.record = line
-                yield self.record
+                yield line
          
     def next_record(self, *args):
-        print(self.record)
-        return self.record
+        self.streaming = args[0]
+        if self.streaming == False:
+            self.file.close()
+            return self
+        else:
+            self.record = args[1]
+            print(self.record)
 
-        
+  
+            
 class Trim(Filter):
     """For each line of input, Trim produces one line of output trimmed to given number of elements"""
     def __init__(self, ntrim=5):
@@ -55,52 +54,59 @@ class Trim(Filter):
         self.trim = []
         
     def next_record(self, *args):
-        record = args[0]
-        if record == 'EOF':
-            return
-        self.trim = record[:self.ntrim]
-        print(self.trim)  
+        self.streaming = args[0]
+        if self.streaming == False:
+            return self
+        else:
+            record = args[1]
+            self.trim = record[:self.ntrim]
+            print(self.trim)  
 
-        
+  
+            
 class Head(Filter):
     """Echos the first n lines (default = 5) of input unchanged, then stops producing output"""
     def __init__(self, nhead=5):
         Filter.__init__(self)
         self.nhead = nhead
+        self.head = []
 
         
     def next_record(self, *args):
-        record = args[0]
-        index = args[1]
-        if record == 'EOF':
-            return
-        if index < self.nhead:
-            if index == 0:
-                self.head = record
-            else:
-                self.head = np.vstack((self.head, record))
-            print(record)    
-        
+        self.streaming = args[0]
+        if self.streaming == False:
+            return self
+        else:
+            record = args[1]
+            index = args[2]
+            type(index)
+            if index < self.nhead:
+                if index == 0:
+                    self.head = record
+                else:
+                    self.head = np.vstack((self.head, record))
+                print(record)    
 
+                
+                
     
 class Sort(Filter):
     """Waits until it has read all of the input, then starts producing output"""
     def __init__(self):
         Filter.__init__(self)
+        self.sort = []
         
     def next_record(self, *args):
-        record = args[0]
-        index = args[1]            
-        if index == 0:
-            self.sort = record
-        elif record == 'EOF':
-            print(self.sort)
-            return
+        self.streaming = args[0]
+        if self.streaming == False:
+            print(*self.sort,sep='\n')
+            return self
         else:
-            self.sort = np.vstack((self.sort, record))
-
-
-       
+            record = args[1]   
+            self.sort.append(record)
+        
+        
+             
     
 class Pipe(Filter):
     """Given a bunch of objects, Pipe puts them in a list and starts running them"""
@@ -112,10 +118,9 @@ class Pipe(Filter):
         self.record = []
         self.filter_list = []
         self.data = []
-        if len(args) < 1:
+        if len(args) <= 1:
             args = Reader(), Trim(), Head(), Sort()
         self.filter_list = args
-    
 
 
         
@@ -124,22 +129,19 @@ class Pipe(Filter):
         """RUN runs the filters Reader, Trim, Head, and Sort and 
         outputs the results of the last input filter
         
-        ## Parameters
-        
-        ### Input
-        
-        None
-        
-        ## Output
-        
         data:  the output of the last filter run (default is Sort)  
         """
-        while self.streaming == True:
+        streaming = self.streaming
+        while streaming == True:
             for index, record in enumerate(self.filter_list[0]):
                 for filter in self.filter_list:
-                    filter.next_record(record, index)
-            self.streaming = False
-            return
+                    filter.next_record(streaming, record, index)
+            streaming = False
+            self.streaming == False
+        else:
+            for filter in self.filter_list:
+                filter.next_record(streaming, record, index)
+        return self
             
             
         
