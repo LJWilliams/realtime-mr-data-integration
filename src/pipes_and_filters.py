f32e395db7df55a02161d3d158fdff085959523a
Lynne Williams <williams.lynne99@gmail.com>
2017-01-11 00:17:05 -0800

    now everything except sort works on multiple different text documents #1

--- src/pipes_and_filters.py
+++ src/pipes_and_filters.py
@@ -16,38 +16,33 @@ np.set_printoptions(threshold=np.inf)
 
 class Filter():
     """Base class that knows how to read and write data streams. Likely to end up as a queue of some sort"""
-    def __init__(self, text='haiku.txt'):
+    def __init__(self):
         self.datagen = []
-        self.streaming = False
-        self.record = []
-    
-    def collect_data(self):
         self.streaming = True
-        self.datagen = self.create_data()
-        
-        
-    def create_data(self):
-        with open(self.text) as file:
-            for line in file:
-                time.sleep(.25)
-                yield line    
-        file.close()
+        self.record = []
+        self.index = []
+    
+   # To come soon!
                 
         
         
 class Reader(Filter):
     """Class that produces output of Filter. Returns single record.
     """
-    def __init__(self, text='haiku.txt'):
-        self.text = text
-        self.record = []
+    def __init__(self, text):
         Filter.__init__(self)
-        Filter.collect_data(self)
+        self.file = text
+        self.record = []
         
         
-        
+    def __iter__(self):
+        with open(self.file) as file:
+            for line in file:
+                time.sleep(.25)
+                self.record = line
+                yield self.record
+         
     def next_record(self, *args):
-        self.record = args[0]
         print(self.record)
         return self.record
 
@@ -56,7 +51,6 @@ class Trim(Filter):
     """For each line of input, Trim produces one line of output trimmed to given number of elements"""
     def __init__(self, ntrim=5):
         Filter.__init__(self)
-        Filter.collect_data(self)
         self.ntrim = ntrim
         self.trim = []
         
@@ -72,7 +66,6 @@ class Head(Filter):
     """Echos the first n lines (default = 5) of input unchanged, then stops producing output"""
     def __init__(self, nhead=5):
         Filter.__init__(self)
-        Filter.collect_data(self) 
         self.nhead = nhead
 
         
@@ -94,7 +87,6 @@ class Sort(Filter):
     """Waits until it has read all of the input, then starts producing output"""
     def __init__(self):
         Filter.__init__(self)
-        Filter.collect_data(self)
         
     def next_record(self, *args):
         record = args[0]
@@ -110,21 +102,20 @@ class Sort(Filter):
 
        
     
-class Pipe(Reader, Trim, Head, Sort):
+class Pipe(Filter):
     """Given a bunch of objects, Pipe puts them in a list and starts running them"""
     def __init__(self, *args):
-        Reader.__init__(self)
-        Trim.__init__(self)
-        Head.__init__(self)
-        Sort.__init__(self)
+        if not isinstance(args[0], Reader):
+            arg = Reader()
+            arg.append(args)
+        Filter.__init__(self)
         self.record = []
         self.filter_list = []
         self.data = []
-        
         if len(args) < 1:
             args = Reader(), Trim(), Head(), Sort()
         self.filter_list = args
-
+    
 
 
         
@@ -144,7 +135,7 @@ class Pipe(Reader, Trim, Head, Sort):
         data:  the output of the last filter run (default is Sort)  
         """
         while self.streaming == True:
-            for index, record in enumerate(self.datagen):
+            for index, record in enumerate(self.filter_list[0]):
                 for filter in self.filter_list:
                     filter.next_record(record, index)
             self.streaming = False
