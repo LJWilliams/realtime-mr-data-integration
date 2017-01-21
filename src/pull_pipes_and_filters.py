d28a18fbd07c61d7766557b58568d3f9c5c7cba6
Lynne Williams <williams.lynne99@gmail.com>
2017-01-20 18:00:13 -0800

    Reader and Trim go through while loop and while loop stops when requested.  Head and Sort to come.

--- src/pull_pipes_and_filters.py
+++ src/pull_pipes_and_filters.py
@@ -12,7 +12,7 @@ and print that as output.
 Input is a txt file (strings) and returns a string delimited by |.
 """
 
-import time
+#import polling
 
 class Filter():
     """Base class of all filters, intended for use in Pipe.
@@ -24,40 +24,38 @@ class Filter():
 
     def finish(self): 
         return None
-     
     
+    def get_record(self, filters):
+        index = [ filter.title for filter in filters ].index(self.title)
+        if filters[index-1].title != self.title:
+            status, record = filters[index-1].next_record(filters)
+        return status, record
+
+
 class Reader(Filter):
     """Produce lines of text from a file one by one.
     """
 
     def __init__(self, filename):
         super().__init__('Reader')
-        self.filename = filename
+        self.file = open(filename)
       
     def finish(self):
         self.file.close()
         return self
         
-    def next_record(self, stream, record):
-        if stream == 'Open':
-            # if closed or doesn't exist, open self.filename
-            try:
-                if self.file.closed: 
-                    self.file = open(self.filename)
-            except AttributeError:
-                self.file = open(self.filename)
-            # get the record, or if there is none, return None
+    def next_record(self, filters):
+        if not self.file.closed:
+            status = 'Open'
             try:
                 record = self.file.readlines(1)[0]
             except IndexError:
-                return stream, None
-            return stream, record  
-        elif stream == 'Closed':
-            if record != None:
                 self = Reader.finish(self)
-                return stream, record
-            elif record == None:
-                return stream, None
+                return 'Closed', None
+            return status, record  
+        else:
+            return 'Closed', None
+
 
 class Trim(Filter):
     """Trim input records to 'ntrim' columns or less."""
@@ -66,19 +64,25 @@ class Trim(Filter):
         super().__init__('Trim')
         self.ntrim = ntrim
         self.trim = []
+        self.filter2call = None
           
-    def next_record(self, stream, record):
-        if stream == 'Open':
-            if record != None:
+    def next_record(self, filters):
+        status, record = super().get_record(filters)
+             
+        if status == 'Open':
+            if record != None and record.count('\n') == 1:
                 self.trim.append(record[:self.ntrim])
-                return stream, None
+                return  'Open', None
+            elif record != None and record.count('\n') > 1:
+                return 'Closed', ' | '.join(item[:self.ntrim] for item in record.split(' | '))
             elif record == None:
-                return stream, None
-        elif stream == 'Closed':
+                return 'Open', None
+        elif status == 'Closed':
             if len(self.trim) == 0:
-                return stream, ' | '.join(item[:self.ntrim] for item in record.split(' | '))
+                return  status, ' | '.join(item[:self.ntrim] for item in record.split(' | '))
             else:
-                return stream, ' | '.join(self.trim)  
+                return  status, ' | '.join(self.trim)
+
 
                 
 class Head(Filter):
@@ -87,25 +91,26 @@ class Head(Filter):
 
     def __init__(self, nhead=5):
         super().__init__('Head')
+        super().poll_filter()
         self.nhead = nhead
         self.head = []
         self.index = 0
         
         
-    def next_record(self, stream, record):
-        if stream == 'Open':
+    def next_record(self, status, record):
+        if status == 'Open':
             if record == None:
-                return stream, None
+                return status, None
             elif record != None:
                 if self.index <= self.nhead:
                     self.head.append(record)
                 self.index += 1
-                return stream, None
-        elif stream == 'Closed':
+                return status, None
+        elif status == 'Closed':
             if len(self.head) == 0:
-                return stream, ' | '.join(record.split(' | ')[:self.nhead])
+                return status, ' | '.join(record.split(' | ')[:self.nhead])
             else:
-                return stream, ' | '.join(self.head[:self.nhead])
+                return status, ' | '.join(self.head[:self.nhead])
 
 
                 
@@ -115,21 +120,21 @@ class Sort(Filter):
 
     def __init__(self):
         super().__init__('Sort')  
+        super().poll_filter()
         self.sort = []
 
-    def next_record(self, stream, record):
-        print(stream, record)
-        if stream == 'Open':
+    def next_record(self, status, record):
+        if status == 'Open':
             if record == None:
-                return stream, None
+                return status, None
             elif record != None:
                 self.sort.append(record)
-                return stream, None
-        elif stream == 'Closed':
+                return status, None
+        elif status == 'Closed':
             if len(self.sort) == 0:
-                return stream, ' | '.join(sorted(record.split(' | ')))       
+                return status, ' | '.join(sorted(record.split(' | ')))       
             else:
-                return stream, ' | '.join(sorted(self.sort))
+                return status, ' | '.join(sorted(self.sort))
 
 class Pipe():
     """Given a bunch of filters, run them in order.
@@ -137,32 +142,18 @@ class Pipe():
 
     def __init__(self, *args):
         self.filters = args 
-        
- 
 
     def run(self):
         """Run the given filters in order, passing the output of each to the next.
         """
-
-        record = None
-        stream = 'Open' # Needs to send command to Filter to open file
-        print('Use Ctrl-C to exit') # How to shut down loop
+        
         while True:
-            try:
-                for filter in self.filters:
-                    stream, record = filter.next_record(stream, record)
-                    time.sleep(.1)
-                    #print(filter.title, stream, record)
-                if stream == 'Closed':
-                    return record
-            except KeyboardInterrupt:
-                stream = 'Closed'
-                pass
+            #for filter in self.filters:
+            status, record = self.filters[-1].next_record(self.filters)
+            print(status, record)
+            #print(record)
+            if status == 'Closed':
+                return record
+
             
-                
-
-                
-        
-            #raise stored_exception[0], stored_exception[1], stored_exception[2]
-        
-
+       
