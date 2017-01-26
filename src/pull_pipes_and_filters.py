b14775e87e36f212d9a4213b6363701ffa849a54
Greg Wilson <gvwilson@third-bit.com>
2017-01-26 08:33:37 -0500

    Comments

--- src/pull_pipes_and_filters.py
+++ src/pull_pipes_and_filters.py
@@ -20,14 +20,30 @@ class Filter():
         """Construct common elements of all filters."""
         self.title = title 
 
+
     def finish(self): 
         return None
     
+
     def get_record(self, filters):
+
+# FIXME: this is doing a search linear in the length of the pipeline
+# for each record.  How about giving each Filter an 'upstream' member
+# (set by Pipe when the Filters are constructed or when they're put
+# into the pipeline)?
+
         index = [ filter.title for filter in filters ].index(self.title)
         if filters[index-1].title != self.title:
-            status, record = filters[index-1].next_record(filters)
+            status, record = filters[index-1].next_record(filters) # FIXME: see comment below
         return status, record
+
+# FIXME: since every Filter child class is supposed to have a
+# next_record method, I'd define something like this here:
+#
+# def next_record(self):
+#   raise NotImplementedError('derived class must implement next_record')
+#
+# with a docstring explaining the return protocol (string plus record).
 
 
 class Reader(Filter):
@@ -35,20 +51,26 @@ class Reader(Filter):
     """
 
     def __init__(self, filename):
-        super().__init__('Reader')
+        super().__init__('Reader') # Good.
         self.file = open(filename)
       
+
     def finish(self):
-        self.file.close()
+        self.file.close() # Good.
         return self
+
+# FIXME: I know I said explicit return, but I think what I meant was
+# either "all returns explicitly return a value" or "there are no
+# returns".  Sorry for not being clear.
         
+
     def next_record(self, filters):
         if not self.file.closed:
             status = 'Open'
             try:
                 record = self.file.readlines(1)[0]
             except IndexError:
-                self = Reader.finish(self)
+                self = Reader.finish(self) # FIXME: whoa, no, this wants to be 'self.finish()'
                 return 'Closed', None
             return status, record  
         else:
@@ -61,14 +83,21 @@ class Trim(Filter):
     def __init__(self, ntrim=5):
         super().__init__('Trim')
         self.ntrim = ntrim
-        self.trim = []
-        self.filter2call = None
+        self.trim = [] # FIXME: why accumulate? why not return one by one as records come in?
+        self.filter2call = None # FIXME: unused
           
+
     def next_record(self, filters):
         status, record = super().get_record(filters)
+
+# FIXME: you can (and should) just say 'self.get_record(filters)',
+# since this child class inherits all the methods of the parent class.
+#
+# FIXME: you won't need to pass in 'filters' once you're caching the
+# upstream filter.
              
         if status == 'Open':
-            if record != None and record.count('|') == 0:
+            if record != None and record.count('|') == 0: # FIXME: whoa, what's the '|' for?
                 self.trim.append(record[:self.ntrim])
                 return  'Open', None
             elif record != None and record.count('|') >= 1:
@@ -82,7 +111,7 @@ class Trim(Filter):
                 return  status, ' | '.join(self.trim)
 
 
-                
+
 class Head(Filter):
     """Echo the first N lines of input.
     """
@@ -90,14 +119,14 @@ class Head(Filter):
     def __init__(self, nhead=5):
         super().__init__('Head')
         self.nhead = nhead
-        self.head = []
+        self.head = [] # FIXME: again, why accumulate? Why not return one by one?
         self.index = 0
         
         
     def next_record(self, filters):
-        status, record = super().get_record(filters)
+        status, record = super().get_record(filters) # FIXME: as above
         if status == 'Open':
-            if record != None and record.count('|') == 0:
+            if record != None and record.count('|') == 0: # FIXME: again, what's the '|' ?
                 #if self.index <= self.nhead:
                 self.head.append(record)
                 self.index += 1
@@ -120,7 +149,7 @@ class Sort(Filter):
 
     def __init__(self):
         super().__init__('Sort')  
-        self.sort = []
+        self.sort = [] # FIXME: yes, this one needs to accumulate
 
     def next_record(self, filters):
         status, record = super().get_record(filters)
@@ -154,5 +183,6 @@ class Pipe():
             if status == 'Closed':
                 return record
 
-            
-       
+# FIXME: if I follow the logic here correctly, this loops until the
+# last filter says 'Closed', then returns a single record - unless the
+# filters are always return records in lists?
